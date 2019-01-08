3237349c0ada598a2efa35e43ab908e2bed7faae
jiangcm <jiangcm@mapbar.com>
2019-01-08 14:38:25 +0800

    匹配自己食材数量

--- cloudfunctions/dishDetail/index.js
+++ cloudfunctions/dishDetail/index.js
@@ -4,11 +4,15 @@ const cloud = require('wx-server-sdk')
 cloud.init()
 
 // 云函数入口函数
-exports.main = async(event, context) => {
+exports.main = async (event, context) => {
   console.log('parameter start----------------');
-  console.log(event.dishId);
+  console.log('dishId=' + event.dishId);
+  console.log('openid=' + event.openid);
+  console.log('openidOthers=' + event.openidOthers);
   console.log('parameter  end ----------------');
 
+  var openid = event.openid;
+  var openidOthers = event.openidOthers;
   const db = cloud.database();
   const dishResult = await db.collection('dish').where({
     _id: event.dishId
@@ -19,6 +23,16 @@ exports.main = async(event, context) => {
     dishId: event.dishId
   }).get();
   const recipes = recipesResult.data;
+  console.log(recipes)
+  // 自己所拥有的食材-jcm start
+  var ingredientList = new Array();
+  if (openid != openidOthers) {
+    const ingredientListResult = await db.collection('ingredient').where({
+      openid: openid
+    }).get();
+    ingredientList = ingredientListResult.data;
+  }
+  // 自己所拥有的食材-jcm end
 
   var retList = new Array();
   for (var i = 0; i < recipes.length; i++) {
@@ -27,10 +41,25 @@ exports.main = async(event, context) => {
     }).get();
     const ingredient = ingredientResult.data;
     if (ingredient.length > 0) {
+      // 匹配自己食材数量-jcm start
+      var quantity = 0;
+      if (openid != openidOthers) {
+        for (var y = 0; y < ingredientList.length; y++) {
+          if (recipes[i].ingredientId == ingredientList[y]._id) {
+            quantity = ingredientList[y].quantity;
+            console.log(quantity)
+            break;
+          }
+        }
+      } else {
+        quantity = ingredient[0].quantity
+      }
+      // 匹配自己食材数量-jcm end
       retList[i] = {
         id: recipes[i].ingredientId,
         name: ingredient[0].name,
-        quantity: ingredient[0].quantity
+        // quantity: ingredient[0].quantity
+        quantity: quantity
       };
     } else {
       retList[i] = {
